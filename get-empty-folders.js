cbc076ca691b4a6c151636fdaf4c2cc83ad84e2c
ubogov <s.ubogov@skbkontur.ru>
2018-08-22 18:24:16 +0500

    safe statSync

--- get-empty-folders.js
+++ get-empty-folders.js
@@ -16,9 +16,22 @@ const getAllEmptyDirs = dir => {
 	let files = fs.readdirSync(dir);
 	if (dirIsEmpty(files)) return dir;
 
-	let folders = files.filter(file => fs.statSync(path.join(dir, file)).isDirectory());
+	let folders = files.filter(file => {
+		if (!~file.indexOf('tmp') || !~file.indexOf('node_modules')) return false;
+		try {
+			return fs.statSync(path.join(dir, file)).isDirectory()
+		} catch(e) {
+			return false;
+		}
+	});
 	return folders.reduce((emptyFolders, dirName) =>
 		emptyFolders.concat(getAllEmptyDirs(path.join(dir, dirName))), []);
 }
-console.info('Current folders is empty:');
-getAllEmptyDirs(fullpath).forEach((dir, index) => console.info(`${index+1}. ${dir}`))
+
+const emptyFolders = getAllEmptyDirs(fullpath);
+if (emptyFolders.length) {
+	console.info('Current folders is empty:');
+	getAllEmptyDirs(fullpath).forEach((dir, index) => console.info(`${index+1}. ${dir}`))
+} else {
+	console.info('empty folders not found, good job! :)');
+}
