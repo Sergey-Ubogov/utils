3d187d1cb545555f7cd2682a96c54b3520ecb1df
ubogov <s.ubogov@skbkontur.ru>
2018-08-22 17:02:36 +0500

    add rename-js-to-ts

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # utils
+
+## 1. rename-js-to-ts
+### recursively renames *.js to *.ts and *.jsx to *.tsx files in folder
+### example: node .\rename-js-to-ts.js C:\Users\s.ubogov\Desktop\d
---
+++ rename-js-to-ts.js
@@ -0,0 +1,26 @@
+let fs = require('fs');
+let path = require('path');
+ 
+if (process.argv.length <= 2) {
+    console.log("arg: full path dir for rename *.js to *.tsx");
+    process.exit(-1);
+}
+ 
+var fullpath = process.argv[2];
+ 
+const getAllFiles = dir =>
+  fs.readdirSync(dir).reduce((files, file) => {
+    const name = path.join(dir, file);
+    const isDirectory = fs.statSync(name).isDirectory();
+    return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+  }, []);
+  
+getAllFiles(fullpath).forEach(file => {
+    let arr = file.split('.');
+    if (file.indexOf('node_modules') !== -1) return;
+	let extFile = arr[arr.length - 1]
+	let nameFile = arr.slice(0, -1).join('.');
+	if (file !== __filename) 
+		if (extFile === 'js') fs.renameSync(path.normalize(file), `${nameFile}.ts`);
+		else if (extFile === 'jsx') fs.renameSync(path.normalize(file), `${nameFile}.tsx`)
+});
